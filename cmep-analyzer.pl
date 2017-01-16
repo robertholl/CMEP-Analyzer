b028c3efb650d382d97fef88d65fa70dcb31d9b1
Robert <robert.holl@gmail.com>
2017-01-16 08:28:13 -0500

    Create readinputfile and openoutputfile functions

--- cmep-analyzer.pl
+++ cmep-analyzer.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 require './filemanager.pl';
 readconfig();
+readinputfile();
 print "Input file is: $cmepfile \n";
 print "Output file is: $stats  \n";
--- filemanager.pl
+++ filemanager.pl
@@ -1,10 +1,18 @@
 #!/usr/bin/perl
 use Config::Properties;
 sub readconfig(){
-open my $config, '<', './config.properties' or die "Unable to open the properties file.";
-$properties = Config::Properties->new();
-$properties->load($config);
-$cmepfile = $properties->getProperty('inputfile.name');
-$stats = $properties->getProperty('outputfile.name');
+	open my $config, '<', './config.properties' or die "Unable to open the properties file.";
+	$properties = Config::Properties->new();
+	$properties->load($config);
+	$cmepfile = $properties->getProperty('inputfile.name');
+	$stats = $properties->getProperty('outputfile.name');
+}
+
+sub readinputfile(){
+	open(inputfile, '<', $properties->getProperty('inputfile.name') or die "Unabe to open ". $properties->getProperty('inputfile.name')).".\n";
+}
+
+sub openoutputfile(){
+	open(inputfile, '>', $properties->getProperty('outputfile.name') or die "Unabe to open ". $properties->getProperty('outputfile.name')).".\n";	
 }
 1;
