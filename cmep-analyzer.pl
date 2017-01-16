d4272b80029d785e15d78dd91e05d89a22ce40cc
Robert <robert.holl@gmail.com>
2017-01-16 00:08:48 -0500

    Implement config properties

--- cmep-analyzer.pl
+++ cmep-analyzer.pl
@@ -1,1 +1,4 @@
 #!/usr/bin/perl
+require './filemanager.pl';
+print "Input file is: $cmepfile \n";
+print "Output file is: $stats \n";
--- filemanager.pl
+++ filemanager.pl
@@ -1,1 +1,9 @@
 #!/usr/bin/perl
+use Config::Properties;
+
+open my $config, '<', '../config.properties' or die "Unable to open the properties file.";
+my $properties = Config::Properties->new();
+$properties->load($config);
+my $cmepfile = $properties->getProperty('inputfile.name');
+my $stats = $properties->getProperty('outputfile.name');
+1;
