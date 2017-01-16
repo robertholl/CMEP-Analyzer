6e178b067036346f6597b9825c6db4d4d0e27d4b
Robert <robert.holl@gmail.com>
2017-01-16 14:58:07 -0500

    Added loop to read input file

--- cmep-analyzer.pl
+++ cmep-analyzer.pl
@@ -6,6 +6,9 @@ openoutputfile();
 print "Input file is: $cmepfile \n";
 print "Output file is: $stats  \n";
 my $row = <$inputfile>;
-print "$row\n";
-print $outputfile "$row\n";
+while(my $row = <$inputfile>){
+	chomp $row;
+	print $outputfile "$row\n";
+	print "$row\n";
+}
 closefiles();
